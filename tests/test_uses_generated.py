2d0efa2cb80937d5fb569b2d870f823221761feb
Valentine Svensson <valentine.svensson@scilifelab.se>
2012-07-13 14:55:08 +0200

    Added test for empty string in sys.path

--- tests/test_uses_generated.py
+++ tests/test_uses_generated.py
@@ -1,12 +1,19 @@
 import os
+import sys
+
 import module.uses_generated
 
 
-def test_1_import_wrong_dir():
+def test_1_empty_str_in_sys():
+    assert "" in sys.path, "sys.path does not contains empty string"
+
+
+def test_2_import_wrong_dir():
+    print(sys.path)
     os.chdir("..")
     assert module.uses_generated.try_import() == 0, "wrongly imported"
 
 
-def test_2_import_right_dir():
+def test_3_import_right_dir():
     os.chdir("workdir")
     assert module.uses_generated.try_import() == 1, "failed to import"
